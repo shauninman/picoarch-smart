11a1c043605247a4df0b275538d10bef70ff7a66
Shaun Inman <shaun@panic.com>
2022-10-30 11:32:40 -0400

    additional cores and overrides

--- overrides.c
+++ overrides.c
@@ -1,35 +1,47 @@
 #include "overrides.h"
 #include "overrides/beetle-pce-fast.h"
 #include "overrides/bluemsx.h"
+#include "overrides/fake-08.h"
 #include "overrides/fceumm.h"
 #include "overrides/fmsx.h"
 #include "overrides/gambatte.h"
 #include "overrides/gme.h"
 #include "overrides/gpsp.h"
 #include "overrides/mame2000.h"
+#include "overrides/mednafen_supafaust.h"
+#include "overrides/mgba.h"
+#include "overrides/nxengine.h"
 #include "overrides/pcsx_rearmed.h"
 #include "overrides/picodrive.h"
+#include "overrides/pokemini.h"
 #include "overrides/quicknes.h"
 #include "overrides/smsplus.h"
 #include "overrides/snes9x2002.h"
 #include "overrides/snes9x2005.h"
+#include "overrides/snes9x2005_plus.h"
 #include "util.h"
 
 static const struct core_override overrides[] = {
 	beetle_pce_fast_overrides,
 	bluemsx_overrides,
+	fake_08_overrides,
 	fceumm_overrides,
 	fmsx_overrides,
 	gambatte_overrides,
 	gme_overrides,
 	gpsp_overrides,
 	mame2000_overrides,
+	mednafen_supafaust_overrides,
+	mgba_overrides,
+	nxengine_overrides,
 	pcsx_rearmed_overrides,
 	picodrive_overrides,
+	pokemini_overrides,
 	quicknes_overrides,
 	smsplus_overrides,
 	snes9x2002_overrides,
 	snes9x2005_overrides,
+	snes9x2005_plus_overrides,
 };
 
 static const struct core_override *override;
---
+++ overrides/pokemini.h
@@ -0,0 +1,47 @@
+#include "overrides.h"
+
+static const struct core_override_option pokemini_core_option_overrides[] = {
+	{
+		.key = "pokemini_video_scale",
+		.default_value = "3x",
+	},
+	{
+		.key = "pokemini_palette",
+		.default_value = "Old",
+	},
+	{
+		.key = "pokemini_piezofilter",
+		.default_value = "disabled",
+	},
+	{
+		.key = "pokemini_lowpass_filter",
+		.default_value = "enabled",
+	},
+	{
+		.key = "pokemini_60hz_mode",
+		.default_value = "enabled",
+	},
+	{ NULL }
+};
+
+me_bind_action pokemini_ctrl_actions[] =
+{
+	{ "UP       ",  1 << RETRO_DEVICE_ID_JOYPAD_UP},
+	{ "DOWN     ",  1 << RETRO_DEVICE_ID_JOYPAD_DOWN },
+	{ "LEFT     ",  1 << RETRO_DEVICE_ID_JOYPAD_LEFT },
+	{ "RIGHT    ",  1 << RETRO_DEVICE_ID_JOYPAD_RIGHT },
+	{ "A BUTTON ",  1 << RETRO_DEVICE_ID_JOYPAD_A },
+	{ "B BUTTON ",  1 << RETRO_DEVICE_ID_JOYPAD_B },
+	{ "C BUTTON ",  1 << RETRO_DEVICE_ID_JOYPAD_R },
+	{ "SHAKE    ",  1 << RETRO_DEVICE_ID_JOYPAD_L },
+	{ "POWER    ",  1 << RETRO_DEVICE_ID_JOYPAD_SELECT },
+	{ NULL,       0 }
+};
+
+#define pokemini_overrides {                               \
+	.core_name = "pokemini",                               \
+	.actions = pokemini_ctrl_actions,                      \
+	.action_size = array_size(pokemini_ctrl_actions),      \
+	.options = pokemini_core_option_overrides              \
+}
+
